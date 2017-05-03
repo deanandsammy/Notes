a53ee8d8fc28c80b4aec7cf0df69e1eb2f93649e
Vic Hu <hkissbigapple@163.com>
2017-05-03 17:39:34 +0800

    typeDetection and parameters collection

--- JS/JavaScript-meta-skill/src/type-detection.js
+++ JS/JavaScript-meta-skill/src/type-detection.js
@@ -1,1 +1,23 @@
-var a = 1;
+/*
+* 类型检测
+* @param anytype
+* @author huge
+* @date May 3，2017
+*/
+
+function typeDetection(target) {
+    return Object.prototype.toString.call(target).slice(8, -1).toLowerCase()
+}
+
+// 函数参数收集
+// @date May 3，2017
+
+function paramsCollection() {
+    var sliceFunc = Function.prototype.call.bind(Array.prototype.slice)
+
+    return sliceFunc(arguments)
+}
+
+function paramsCollection2() {
+    return Array.prototype.slice.call(arguments)
+}
